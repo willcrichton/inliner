d3282053c5984ecbac6b762c8b97b9dc4df0d09b
Will Crichton <wcrichto@cs.stanford.edu>
2020-03-12 15:43:40 -0700

    Second try for syntax

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setup(name='inliner',
-          version='0.3.12',
-          description='Human-readable inlining of Python code',
-          url='http://github.com/willcrichton/inliner',
-          author='Will Crichton',
-          author_email='wcrichto@cs.stanford.edu',
-          license='Apache 2.0',
-          packages=find_packages(),
-          install_requires=[
-              'astor', 'iterextras', 'asttokens',
-              'ast_tools @ git+https://github.com/leonardt/ast_tools'
-          ],
-          data_files=[('share/jupyter/nbextensions/inliner', [
-              'inliner_jupyter/dist/notebook.js',
-              'inliner_jupyter/dist/notebook.js.map',
-          ]),
-                      ('etc/jupyter/nbconfig/notebook.d',
-                       ['inliner_jupyter/inliner.json'])],
-          setup_requires=['pytest-runner'],
-          tests_require=['pytest', 'seaborn', 'pandas', 'numpy'],
-          zip_safe=False)
+    setup(
+        name='inliner',
+        version='0.3.12',
+        description='Human-readable inlining of Python code',
+        url='http://github.com/willcrichton/inliner',
+        author='Will Crichton',
+        author_email='wcrichto@cs.stanford.edu',
+        license='Apache 2.0',
+        packages=find_packages(),
+        install_requires=[
+            'astor', 'iterextras', 'asttokens',
+            'ast_tools @ git+https://github.com/leonardt/ast_tools.git#egg=ast_tools-0.0.14'
+        ],
+        data_files=[('share/jupyter/nbextensions/inliner', [
+            'inliner_jupyter/dist/notebook.js',
+            'inliner_jupyter/dist/notebook.js.map',
+        ]), ('etc/jupyter/nbconfig/notebook.d',
+             ['inliner_jupyter/inliner.json'])],
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest', 'seaborn', 'pandas', 'numpy'],
+        zip_safe=False)
